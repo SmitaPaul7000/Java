c8f7cdd38d5ae91a82b3bb31daefb5274d056ad6
Smita Paul <49784273+SmitaPaul7000@users.noreply.github.com>
2023-06-07 14:29:31 +0530

    Collection and Generics

---
+++ JavaBasic/src/main/java/org/example/collections/collection_03.java
@@ -0,0 +1,21 @@
+package org.example.collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class collection_03 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ArrayList<Integer> intarr = new ArrayList<>();
+        HashMap<String, Integer> intMap = new HashMap<>();
+
+        for (Integer i : intarr) {
+            System.out.println(i);
+        }
+
+        for (String key : intMap.keySet()) {
+            Integer i = intMap.get(key);
+        }
+        intarr.iterator();
+    }
+}
---
+++ JavaBasic/src/main/java/org/example/generics/Generics_collections.java
@@ -0,0 +1,53 @@
+package org.example.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Generics_collections {
+    public static void main(String[] args) {
+
+        Truck t = new Truck();
+        t.insert(new Cement());
+        ArrayList<Sand> sandBags = new ArrayList<>(2);
+        sandBags.add(new Sand());
+        sandBags.add(new Sand());
+        t.insertAll(sandBags);
+        t.printStatus();
+    }
+
+    static class Goods {}
+    static class Cement extends Goods {}
+    static class Sand extends Goods {}
+    static class Person {}
+    static class Student extends Person {}
+
+    static class Vehicle<T> {
+        Stack<T> items = new Stack<>();
+        int capacity = 10;
+        private int filled = 0;
+        void printStatus() {
+            items.forEach(t -> System.out.println(t));
+        }
+
+        void insertAll(List<? extends T> items) {
+            filled += items.size(); // check if filled <= capacity
+            this.items.addAll(items);
+        }
+
+        void insert(T item) {
+            filled++; // check if filled <= capacity
+            items.add(item);
+        }
+        T unload () {
+            T item = items.pop();
+            filled--; // check if filled >= 0
+            return item;
+        }
+    }
+
+    static class SchoolBus extends Vehicle<Person> {}
+    static class Truck extends Vehicle<Goods> {}
+}
+
+// assignment #02 -> where would we need contravariance ??
